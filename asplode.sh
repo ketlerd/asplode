a41cfc494ed2b07696b54409e469824fba59339f
David Ketler <david@awarebit.com>
2023-10-11 13:00:35 -0400

    sha512crypt

--- asplode.sh
+++ asplode.sh
@@ -10,4 +10,6 @@ rg -N -e '^\$2[ayb]\$.{56}$' $1 > bcrypt.txt
 
 rg -N -e '^[a-fA-F0-9]{128}$' $1 > sha512.txt
 
-rg -N --invert-match -e '^[a-fA-F0-9]{40}$' $1 | rg -N --invert-match -e '^[a-fA-F0-9]{64}$' | rg -N --invert-match -e '^[a-fA-F0-9]{32}$' | rg -N --invert-match -e '^\$2[ayb]\$.{56}$' | rg -N --invert-match -e  '^[a-fA-F0-9]{128}$' > therest.txt
+rg -N -e '^\$6\$rounds=50\$.{86}$' $1 | awk '{sub(/\$6\$rounds=50\$/,"$6$rounds=50$rounds=50$"); print}'> sha512crypt.txt
+
+rg -N --invert-match -e '^[a-fA-F0-9]{40}$' $1 | rg -N --invert-match -e '^[a-fA-F0-9]{64}$' | rg -N --invert-match -e '^[a-fA-F0-9]{32}$' | rg -N --invert-match -e '^\$2[ayb]\$.{56}$' | rg -N --invert-match -e  '^[a-fA-F0-9]{128}$' | rg -N --invert-match -e '^\$6\$rounds=50\$.{86}$' | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- > therest.txt
