a162b42c23aec994216609a43e9d18019d5e2cf8
David Ketler <david@awarebit.com>
2023-10-11 13:54:35 -0400

    fix 5000 rounds

--- asplode.sh
+++ asplode.sh
@@ -12,7 +12,7 @@ rg -N -e '^[a-fA-F0-9]{128}$' $1 > sha512.txt
 
 rg -N -e '^\$6\$rounds=50\$.{86}$' $1 | awk '{sub(/\$6\$rounds=50\$/,"$6$rounds=50$rounds=50$"); print}'> sha512crypt.txt
 
-rg -N -e '^\$6\$rounds=5000\$.{86}$' $1 | awk '{sub(/\$6\$rounds=5000\$/,"$6$rounds=50$rounds=5000$"); print}'>> sha512crypt.txt
+rg -N -e '^\$6\$rounds=5000\$.{16}.{87}$' $1 | awk '{sub(/\$6\$rounds=5000\$/,"$6$rounds=5000$rounds=5000$"); print}'>> sha512crypt.txt
 
 
-rg -N --invert-match -e '^[a-fA-F0-9]{40}$' $1 | rg -N --invert-match -e '^[a-fA-F0-9]{64}$' | rg -N --invert-match -e '^[a-fA-F0-9]{32}$' | rg -N --invert-match -e '^\$2[ayb]\$.{56}$' | rg -N --invert-match -e  '^[a-fA-F0-9]{128}$' | rg -N --invert-match -e '^\$6\$rounds=50\$.{86}$' | rg -N --invert-match -e '^\$6=\$rounds=5000\$.{86}$' | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- > therest.txt
+rg -N --invert-match -e '^[a-fA-F0-9]{40}$' $1 | rg -N --invert-match -e '^[a-fA-F0-9]{64}$' | rg -N --invert-match -e '^[a-fA-F0-9]{32}$' | rg -N --invert-match -e '^\$2[ayb]\$.{56}$' | rg -N --invert-match -e  '^[a-fA-F0-9]{128}$' | rg -N --invert-match -e '^\$6\$rounds=50\$.{86}$' | rg -N --invert-match -e '^\$6\$rounds=5000\$.{16}.{87}$' | awk '{ print length, $0 }' | sort -n | cut -d" " -f2- > therest.txt
